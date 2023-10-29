a5da52cd9cb6232248a285d3bf47d217aa26ec35
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2023-10-29 14:39:02 +0700

    Update cursed_cpp.h
    idk

--- cursed_cpp.h
+++ cursed_cpp.h
@@ -22,6 +22,11 @@
     #define đẩy_bit_qua_bên_phải_thêm >>=
     #define đẩy_bit_qua_bên_trái <<
     #define đẩy_bit_qua_bên_trái_thêm <<=
+    #define và_bit &
+    #define hoặc_bit |
+    #define phủ_định_bit ~ 
+    #define hoặc_bit_loại_trừ ^
+
     // more_here
 
     // comparison operators
@@ -60,20 +65,39 @@
     #define trả_về return
     #define dùng using
     #define không_gian_tên namespace
-
+    #define tư private
+    #define ảo virtual
+    #define công public 
+    #define tự_động auto
+    #define đi_đến goto
+    #define được_bảo_vệ protected
+    #define hợp_nhất union
+    #define ném_trả throw
+    #define tĩnh static
+    #define nội_tuyến inline
+    #define bên_ngoài extern
+    #define xuất export
+    #define nhập import
+    #define kiểu_mẫu template
+    #define rõ_ràng explicit
+    #define liệt_kê enum
+    #define phép_toán operator 
     // data types
     #define số_nguyên int
+    #deifne số_nguyên_nhỏ short int
     #define số_nguyên_lớn long long
     #define số_nguyên_dương unsigned int
     #define số_thực float
     #define số_thực_lớn long double
     #define kí_tự char
+    #define kí_tự_rộng wchar_t
     #define xâu std::string
     #define đúng_sai bool
     #define ĐÚNG true
     #define SAI false
-    #define tự_động auto
+    
     #define vô_định void
+
 #endif
 
 #ifdef VIETNAMESE_MISC
